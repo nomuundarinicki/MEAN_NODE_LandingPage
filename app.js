9a76da79a6a9c5f9209bd61293a7271af4dfcabb
Nick Chimedbavuu <nickchimedbavuu@Nickis-MacBook-Pro.local>
2017-09-06 17:32:27 -0700

    initial

---
+++ app.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (request, response){
+    console.log('client request URL:', request.url);
+    if(request.url === '/'){
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    if(request.url === '/ninjas') {
+        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    if(request.url === '/dojos/new') {
+        fs.readFile('new.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    
+})
+
+server.listen(6789)
+console.log("Running in LOCALHOST at port 6789");
